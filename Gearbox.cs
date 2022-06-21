7955a2e7a51d8836557ed2043dee09b35ab056eb
OscarFreij <otg020313@gmail.com>
2022-06-21 23:39:59 +0200

    Remake Gearbox Object
    Remade Gearbox Object to contain ratioOn and ratioOff.

--- Gearbox.cs
+++ Gearbox.cs
@@ -8,15 +8,19 @@ namespace StormWorks_Gearbox_Calculator
 {
     public class Gearbox
     {
-        public double ratio { get; private set; }
-        public string ratioString { get; private set; }
+        public double ratioOn { get; private set; }
+        public double ratioOff { get; private set; }
+        public string ratioStringOn { get; private set; }
+        public string ratioStringOff { get; private set; }
         public double loss { get; private set; }
 
-        public Gearbox(string ratio, double loss = 0.05)
+        public Gearbox(string ratioOn, string ratioOff = "1/1", double loss = 0.05)
         {
-            this.ratio = (int.Parse(ratio.Substring(0 + ratio.IndexOf('/')))/int.Parse(ratio.Substring(ratio.IndexOf('/') + 1)));
+            this.ratioOn = (int.Parse(ratioOn.Substring(0, ratioOn.IndexOf('/')))/int.Parse(ratioOn.Substring(ratioOn.IndexOf('/') + 1)));
+            this.ratioOff = (int.Parse(ratioOff.Substring(0, ratioOff.IndexOf('/'))) / int.Parse(ratioOff.Substring(ratioOff.IndexOf('/') + 1)));
             this.loss = loss;
-            this.ratioString = ratio;
+            this.ratioStringOn = ratioOn;
+            this.ratioStringOff = ratioOff;
         }
     }
 }
